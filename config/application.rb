cee97ad3150234017c223348916f9ad07c7cf447
togashi193 <iku0425@yahoo.co.jp>
2019-08-12 17:39:48 +0900

    Edit setting

--- config/application.rb
+++ config/application.rb
@@ -33,5 +33,6 @@ module Kakimbo
     # Middleware like session, flash, cookies can be added back manually.
     # Skip views, helpers and assets when generating a new resource.
     config.api_only = true
+    config.hosts << ENV["ENDPOINT"]
   end
 end
