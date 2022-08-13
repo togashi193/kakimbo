f969c5b07c09813501e9ef05c47d0a0fedb7055d
togashi193 <iku0425@yahoo.co.jp>
2022-08-13 23:29:37 +0900

    add update billing

--- app/controllers/billings_controller.rb
+++ app/controllers/billings_controller.rb
@@ -14,6 +14,16 @@ class BillingsController < ApplicationController
     )
   end
 
+  def update
+    @billing = current_user.billings.find_by!(id: params[:id])
+    @billing.update!(
+      game: Game.find(params[:game]),
+      billing_date: params[:date],
+      amount: params[:amount],
+      note: params[:note],
+    )
+  end
+
   def destroy
     current_user.billings.find_by!(id: params[:id]).destroy!
   end
